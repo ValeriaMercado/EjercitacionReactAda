e77eaab1ee0e62e2fc5f603a14b8f2efc9e5b9c2
ValeriaMercado <valeria.mza93@gmail.com>
2023-02-07 19:01:30 -0300

    first commit

---
+++ 6febrero/src/App.jsx
@@ -0,0 +1,11 @@
+import Episodios from "./components/Episodios";
+
+function App() {
+  return (
+    <div>
+      <Episodios />
+    </div>
+  );
+}
+
+export default App;
---
+++ 6febrero/src/components/Episodios.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const Episodios = () => {
+  const [mostrar, setMostrar] = useState(true);
+
+  const [episodios, setEpisodios] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/episode`)
+      .then((res) => res.json())
+      .then((data) => {
+        setEpisodios(data.results);
+      });
+  }, []);
+
+  const [personajes, setPersonajes] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPersonajes(data.results);
+        console.log(data.results);
+      });
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => setMostrar(!mostrar)}>
+        {mostrar === true ? "Personajes" : "Episodios"}
+      </button>
+      {mostrar && episodios.map((p) => <h2>{p.name}</h2>)}
+      {!mostrar && personajes.map((p) => <h2>{p.name}</h2>)}
+    </div>
+  );
+};
+
+export default Episodios;
